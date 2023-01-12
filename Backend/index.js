a0891b551d825b0e8b3ada1a25103b8bc82eea41
Gabriel Clarin <gabrielmclarin@gmail.com>
2023-01-12 11:54:38 -0800

    Finished Code

---
+++ Backend/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+const path = require('path')
+const app = express(); 
+
+var jsonParser= bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended : false})
+
+app.use(urlencodedParser)
+app.use(jsonParser)
+app.use(express.static(path.join(__dirname, '../Frontend')));
+
+
+
+app.get('/', function(req, res) {
+	let reqPath = path.join(__dirname, '../Frontend/index.html');
+	res.sendFile(reqPath);
+});
+
+
+app.post('/chatbot', (req, res) => {
+	const message = req.body.message;
+	const number = message.match(/\d+/);
+	if (number) {
+		fetch(`http://numbersapi.com/${number}?type=trivia`).then(response => response.text()).then(data => {
+			res.json({
+				text: data
+			});
+		}).catch(error => {
+			res.json({
+				text: "Sorry, I couldn't find any information about that number."
+			});
+		});
+	}
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
